cea29caf9686a2217e8433efb9b238e60be90373
Paramesh Gunasekaran <paramesh.gunasekaran@gmail.com>
2023-08-07 19:20:08 +1000

    init: intial version

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Microsoft.SemanticKernel.AI.TextCompletion;
+
+namespace Test
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddJsonFile("appsettings.development.json", true).Build();
+
+                var kernel = new KernelBuilder().WithOpenAITextCompletionService(configuration["modelId"], configuration["apiKey"], configuration["orgId"]).Build();
+
+                var function_definition = "Explain the Physics law '{{$input}}' with examples, like  explaining to a 5 year old kid.";
+
+                var arguments = "Laws of Thermodynamics";
+
+                var function_reference = kernel.CreateSemanticFunction(function_definition);
+
+                function_reference.UseCompletionSettings(new CompleteRequestSettings { MaxTokens = 1024 });
+
+                var function_response = await function_reference.InvokeAsync(arguments);
+
+                Console.WriteLine(function_response.Result);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit...");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
